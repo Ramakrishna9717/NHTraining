d38b3070452f50ec8aa06a618d44a5a93a14ddfa
Ramakrishna9717 <Ramakrishna9717@gmail.com>
2022-01-25 13:14:47 +0530

    first submission

---
+++ day 1 project 4/day 1 project 4/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day_1_project_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int fn = Convert.ToInt32(textBox1.Text);
+            int sn = Convert.ToInt32(textBox2.Text);
+            int power = Convert.ToInt32(Math(fn, sn));
+
+            TextBox3.text = power.ToString();
+        }
+    }
+}
