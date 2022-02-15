82558b1fad79d3d8224b9381514564da380dbdff
Ramakrishna9717 <Ramakrishna9717@gmail.com>
2022-02-15 19:14:51 +0530

    NH Training

---
+++ Day 17/Addemblies/Rk Library/MyBaseClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rk_Library
+{
+    public class MyBaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        internal protected int e;
+
+        public void MybaseclassMethod()
+        {
+            a=10;
+            b=20;
+            c=30;
+            d=40;
+            e=50;
+        }
+    }
+    public class MyDerviedClass :MyBaseClass
+    {
+        public void MyDerviedClassMethod()
+        {
+            a=10;
+            b=20;
+            c=30;
+            d=40;
+            e=50;
+        }
+    }
+    public class MyOtherclass
+    {
+        public void MyOtherClassMethod()
+        {
+            MyBaseClass mb = new MyBaseClass();
+            
+               mb.a= 10;
+               mb.b= 20;
+               mb.c= 30;
+               mb.d= 40;
+               mb.e= 50;
+                
+        }
+   
+    }  
+}
+  
+
+
---
+++ Day 17/Addemblies/public Library/MyPublicDerviedClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rk_Library;
+
+namespace public_Library
+{
+    public class MyPublicDerviedClass :MyBaseClass
+    {
+        public void MypublicDerviedClassMethod()
+        {
+            a=10;
+            b=20;
+            c=30;
+            d=40;
+            e=50;
+        }
+        
+    }
+
+    public class MyPublicOtherClass
+    {
+        public void MyPublicOtherclassMethod()
+        {
+            MyBaseClass mb =new MyBaseClass();
+            mb.a= 10;
+            mb.b= 20;
+            mb.c= 30;
+            mb.d= 40;
+            mb.e= 50;
+        }
+
+    }
+}
