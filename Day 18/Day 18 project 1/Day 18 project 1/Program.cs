5d496259c1d480b83dd82615dba7444818e84f6e
Ramakrishna9717 <Ramakrishna9717@gmail.com>
2022-02-16 21:58:23 +0530

    NH Training

---
+++ Day 18/Day 18 project 1/Day 18 project 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mathametics_LIbrary;
+
+namespace Day_18_project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Algebra.Factorial(7));
+
+            Console.ReadLine();
+        }
+    }
+}
